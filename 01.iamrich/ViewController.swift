d29d0a8a2f13c8884b27111e24be0a06cbe8778d
Arnol Peralta <arnol87.peralta@gmail.com>
2019-06-04 22:35:11 -0500

    Initial Commit

---
+++ 01.iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01.iamrich
+//
+//  Created by Arnol Peralta on 6/4/19.
+//  Copyright © 2019 Arnol Peralta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
